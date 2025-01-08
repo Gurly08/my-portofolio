529af43c323c552122c7d4b8be310fb28a27b2e8
Gurly08 <bagas.dj89@gmail.com>
2025-01-08 21:16:59 +0700

    all section of nav menu done

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleMenu = document.querySelector('.toggle-menu');
+    const menu = document.querySelector('.menu');
+
+    toggleMenu.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+  });
